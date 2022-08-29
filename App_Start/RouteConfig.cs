38aa1ad5a2c8af16d02bbc4ba712b331c5364b81
diegopimi <92610979+diegopimi@users.noreply.github.com>
2022-08-28 17:40:23 -0700

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MyProject
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", //if these parameters aren't set, 
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } //by default we go to home index
+            );
+        }
+    }
+}
---
+++ Filters/VerifySession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyProject.Controllers;
+using MyProject.Models;
+
+namespace MyProject.Filters
+{
+    public class VerifySession : ActionFilterAttribute //heredity
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) //force to do something, enters before access controller since occurs OnAction
+        {
+            var user1 = (user)HttpContext.Current.Session["User"]; //'user' is the field from our table, evaluate session
+            var user2 = (user)HttpContext.Current.Session["User1"]; //'user' is the field from our table, evaluate session
+            if (user1 == null && user2 ==null)
+            {
+       
+                if (filterContext.Controller is AccessController == false) //thus controller is HomeController, without this if, it would cycle all the time in this filter
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/Index"); //then redirect to login.. root
+                }
+
+            }
+            else
+            {
+                if (filterContext.Controller is AccessController == true) 
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index"); 
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
