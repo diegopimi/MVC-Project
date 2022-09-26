96999bfe84294e92877aaac0f17f21b70551a1d1
diegopimi <92610979+diegopimi@users.noreply.github.com>
2022-09-25 20:32:14 -0700

    Update BundleConfig.cs

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace MyProject
 {
     public class BundleConfig
     {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +13,6 @@ namespace MyProject
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
